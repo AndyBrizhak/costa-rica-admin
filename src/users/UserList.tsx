2b90e9de70c869c4d705e4b0911e3cdca8dbb19f
Andrey Brizhak <35806172+AndyBrizhak@users.noreply.github.com>
2026-03-31 18:53:05 +0800

    style(ui): hide ID and empty roles from UserList table

--- src/users/UserList.tsx
+++ src/users/UserList.tsx
@@ -1,33 +1,17 @@
-import {
-  List,
-  Datagrid,
-  TextField,
-  EmailField,
-  ArrayField,
-  SingleFieldList,
-  ChipField,
-} from "react-admin";
+import { List, Datagrid, TextField, EmailField } from "react-admin";
 
 /**
- * Компонент для отображения списка пользователей.
- * Мы используем только необходимые компоненты, чтобы избежать ошибок компиляции.
+ * Чистый список пользователей: только логин и почта.
+ * Мы скрыли ID и роли, чтобы не перегружать интерфейс и не вызывать пустых колонок.
  */
 export const UserList = () => (
   <List>
     <Datagrid rowClick="edit">
-      {/* Уникальный идентификатор пользователя */}
-      <TextField source="id" label="ID" />
-
-      {/* Имя пользователя и Email */}
+      {/* Имя пользователя (логин) */}
       <TextField source="userName" label="Username" />
-      <EmailField source="email" label="Email Address" />
 
-      {/* Роли пользователя: отображаем массив строк как набор графических тегов (Chips) */}
-      <ArrayField source="roles" label="User Roles">
-        <SingleFieldList linkType={false}>
-          <ChipField source="id" size="small" sx={{ fontWeight: "bold" }} />
-        </SingleFieldList>
-      </ArrayField>
+      {/* Электронная почта */}
+      <EmailField source="email" label="Email Address" />
     </Datagrid>
   </List>
 );
